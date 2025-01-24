12c12f10725c6ae6116d210f778701eaf8b2e81f
Splash-Nemo <ap457codes@gmail.com>
2025-01-24 06:01:20 +0530

    Base Frame Set-Up

--- src/gui/BaseFrame.java
+++ src/gui/BaseFrame.java
@@ -1,4 +1,29 @@
 package gui;
+import databaseObjects.Users;
+import javax.swing.*;
 
-public class BaseFrame {
+public class BaseFrame extends JFrame{
+    protected Users user;
+
+    public BaseFrame(String title, Users user){
+        this.user= user;
+        initialize(title);
+    }
+
+    public BaseFrame(String title){
+        initialize(title);
+    }
+
+    private void initialize(String title){
+        setTitle(title);
+        setSize(420,600);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLayout(null);
+        setResizable(false);
+        setLocationRelativeTo(null);
+
+        addGuiComponents();
+    }
+
+    protected abstract void addGuiComponents();
 }
